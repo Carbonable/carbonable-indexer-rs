ae776c001dd7d41d4391ed496589b526f698bdfa
tekkac <98529704+tekkac@users.noreply.github.com>
2023-06-05 17:15:10 +0200

    🗃️ add db migration

--- packages/migration/src/lib.rs
+++ packages/migration/src/lib.rs
@@ -6,6 +6,7 @@ mod m20230420_075445_add_event_store;
 mod m20230427_150756_add_yielder_event_source;
 mod m20230510_153033_add_sale_date;
 mod m20230523_144915_add_forecasted_apr;
+mod m20230601_135331_add_project_value;
 
 pub struct Migrator;
 
@@ -19,6 +20,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230427_150756_add_yielder_event_source::Migration),
             Box::new(m20230510_153033_add_sale_date::Migration),
             Box::new(m20230523_144915_add_forecasted_apr::Migration),
+            Box::new(m20230601_135331_add_project_value::Migration),
         ]
     }
 }
